bb12a92bb2c06eae2d68b8d6861fe6b1d268b2d9
Kuba <kubalobo@gmail.com>
2016-01-21 20:32:51 +0100

    Trochę komentarzy

--- _main.cpp
+++ _main.cpp
@@ -11,28 +11,33 @@ int main()
 	int size = 10;
 
 	char str[10];
-	sprintf(str, "%d", size);
+	sprintf(str, "%d", size);		//zamiana size na string ( 10 na "10" bo w takiej formie lyknie to arrgen )
 
-	int* tablica = new int[size];
-					//argumenty: {pusty, typ R P A D, ilosc, zakresMin, zakresMax}
+	int* tablica = new int[size];	// tutaj moze byc problem ???
+
+					//argumenty: {pusty, typ R P A D, ilosc(size), zakresMin, zakresMax}
 	char* arg[] = 	{"", "A", str, "0", "10"};
 
 	tablica = arrgen(5, arg);
 
-	//arrchk(tablica, size);
+	//arrchk(tablica, size);		//check czy posortowana, chyba dzialal dopoki arrgen sie nie popsul
 
+
+	//testowe wypisanie
 	for (int i = 0; i < size; i++)
-			{
-				cout << tablica[i] << " ";
-			}
+	{
+		cout << tablica[i] << " ";
+	}
 
-	bubble_sort(tablica, size);
+/*
+	//bubble_sort(tablica, size);
 
 	arrchk(tablica, size);
 
 	for (int i = 0; i < size; i++)
-		{
-			cout << tablica[i] << " ";
-		}
+	{
+		cout << tablica[i] << " ";
+	}
+*/
 
 }
