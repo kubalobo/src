6efc0813fd904256b7b4dd2fd67615467d35db0c
Kuba <kubalobo@gmail.com>
2016-01-29 14:12:01 +0100

    kosmetyka

--- _main.cpp
+++ _main.cpp
@@ -10,7 +10,7 @@ using namespace std;
 
 int main()
 {
-	int size = 10000;
+	int size = 100000;
 
 //	cout << "Ilosc elementow: ";
 //	cin >> size;
@@ -32,9 +32,9 @@ int main()
 //----------------------------------------------------
 // Losowanie tablicy ze zbiorem testowym
 	char* arg[] = {(char*)"", (char*)"R", (char*)str, (char*)"0", (char*)str}; cout << "Arg R" << endl; 	//argumenty: {pusty, typ R P A D, ilosc(size), zakresMin, zakresMax}
-	//char* arg[] = {(char*)"", (char*)"P", (char*)str, (char*)"20000"}; cout << "Arg P" << endl;		//partialy sorted - str==size -> ilosc, ilosc inwersji
-	//char* arg[] = {(char*)"", (char*)"A", (char*)str};	cout << "Arg A" << endl;			//rosnaca (posortowana)
-	//char* arg[] = {(char*)"", (char*)"D", (char*)str};	cout << "Arg D" << endl;			//malejaca
+	//char* arg[] = {(char*)"", (char*)"P", (char*)str, (char*)"20000"}; cout << "Arg P" << endl;			//partialy sorted - str==size -> ilosc, ilosc inwersji
+	//char* arg[] = {(char*)"", (char*)"A", (char*)str};	cout << "Arg A" << endl;						//rosnaca (posortowana)
+	//char* arg[] = {(char*)"", (char*)"D", (char*)str};	cout << "Arg D" << endl;						//malejaca
 
 
 	cout << "Losowanie tablicy - ";
